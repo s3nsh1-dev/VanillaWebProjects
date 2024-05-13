3c35929925ec7a1eac9fe1e4dde10370b25330c9
Shubham228197 <shubham228pandey@gmail.com>
2024-05-13 19:14:59 +0530

    Added new Projects and some folder alterations

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -1,19 +1,23 @@
-setInterval(()=>{
+setInterval(() => {
+  const dateTime = new Date();
 
-    d = new Date();
+  hourTime = dateTime.getHours();
+  minTime = dateTime.getMinutes();
+  secTime = dateTime.getSeconds();
 
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
+  hourRotation = 30 * hourTime + minTime / 2;
+  minRotation = 6 * minTime;
+  secRotation = 6 * secTime;
 
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
-
-    document.getElementById("hour").style.transform = `rotate(${hrotation}deg)`
-    document.getElementById("minute").style.transform = `rotate(${mrotation}deg)`
-    document.getElementById("second").style.transform = `rotate(${srotation}deg)`
-
-},1000)
-// its transform not transition to rotate and must use transform-origin  = bottom to move from bottom as center 
+  document.getElementById(
+    "hour"
+  ).style.transform = `rotate(${hourRotation}deg)`;
+  document.getElementById(
+    "minute"
+  ).style.transform = `rotate(${minRotation}deg)`;
+  document.getElementById(
+    "second"
+  ).style.transform = `rotate(${secRotation}deg)`;
+}, 1000);
+// its transform not transition to rotate and must use transform-origin  = bottom to move from bottom as center
 // and its rotate({value}deg) not rotate({value})deg
